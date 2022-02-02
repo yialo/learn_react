32b369d63c82f3a1e062e329640fe5c01078da8e
Aleksei Arro <polsck@gmail.com>
2022-02-02 19:14:52 +0300

    feat: add test suite for Counter component

--- packages/test-utils/src/c_counter/self.test.js
+++ packages/test-utils/src/c_counter/self.test.js
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import {
+  act,
+  isElement,
+  isElementOfType,
+  isDOMComponent,
+  isCompositeComponentWithType,
+} from 'react-dom/test-utils';
 
 import { Counter } from './self';
 
@@ -30,5 +36,42 @@ describe('Counter', () => {
       $button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
     });
     expect($paragraph.textContent).toBe('You clicked 1 times');
+  });
+
+  it('should show that rendered counter element is of Counter type', () => {
+    let counterElement = null;
+
+    act(() => {
+      counterElement = <Counter />;
+    });
+
+    expect(isElementOfType(counterElement, Counter)).toBe(true);
+  });
+
+  it('should show that native div is DOMComponent', () => {
+    const $div = document.createElement('div');
+    expect(isDOMComponent($div)).toBe(true);
+  });
+
+  it('should show that native div is not a React element', () => {
+    const $div = document.createElement('div');
+    expect(isElement($div)).toBe(false);
+  });
+
+  it('should show that ref attached to button contains DOMComponent', () => {
+    const buttonRef = { current: null };
+    act(() => {
+      render(<button ref={buttonRef} />, $container);
+    });
+
+    expect(isDOMComponent(buttonRef.current)).toBe(true);
+  });
+
+  it('should show that ref attached to Counter contains CompositeComponent of Counter type', () => {
+    const counterRef = { current: null };
+    act(() => {
+      render(<Counter ref={counterRef} />, $container);
+    });
+    expect(isCompositeComponentWithType(counterRef.current, Counter)).toBe(true);
   });
 });
