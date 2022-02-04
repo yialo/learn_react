2fdf2f4c1dd2b6beb8fbd49870f45ace780a1327
Aleksei Arro <polsck@gmail.com>
2022-02-04 18:07:51 +0300

    feat: use waitFor in TodoList test case

--- packages/testing-library/src/c_todo-list/self.test.tsx
+++ packages/testing-library/src/c_todo-list/self.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 import { TodoList } from './self';
 
@@ -6,13 +6,11 @@ test('should load todo items and allow to toggle their completion', async () => 
   render(<TodoList />);
 
   // shows preloader at start
-  await screen.findByText('Loading...');
   expect(screen.getByText('Loading...')).toBeVisible();
 
   // loads todo list, then shows it and hide preloader
-  await screen.findByText('My todos:');
+  await waitFor(() => expect(screen.queryByText('Loading...')).toBe(null));
   expect(screen.getByText('My todos:')).toBeVisible();
-  expect(screen.queryByText('Loading')).toBe(null);
 
   // toggles state of todo item on button click
   const firstItemStateDiv = screen.getAllByText('Completed: false')[0];
