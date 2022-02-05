a4e9b58eef2f8fd1a9a0da261ab37ef4de7daccd
Aleksei Arro <polsck@gmail.com>
2022-02-05 23:56:00 +0300

    fix: simplify @testing-library/react-hooks import

--- packages/custom-hooks/src/hooks/use-context-counter.test.tsx
+++ packages/custom-hooks/src/hooks/use-context-counter.test.tsx
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks/dom';
+import { act, renderHook } from '@testing-library/react-hooks';
 
 import { CounterStepProvider, useCounter } from './use-context-counter';
 
