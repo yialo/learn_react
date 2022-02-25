ee74e126d0ada137b34767257efd5420c7e7c493
Aleksei Arro <polsck@gmail.com>
2022-02-25 18:51:07 +0300

    feat: improve types to effect-store-update example

--- packages/effector/src/learn-core/effect-store-update.ts
+++ packages/effector/src/learn-core/effect-store-update.ts
@@ -1,12 +1,14 @@
 import { createEffect, createStore } from 'effector';
 
-const loadPostsFx = createEffect(async () => {
+const loadPostsFx = createEffect<void, unknown[]>();
+
+loadPostsFx.use(async () => {
   const url = `https://jsonplaceholder.typicode.com/posts`;
   const response = await fetch(url);
   return response.json();
 });
 
-const posts$ = createStore<any[]>([]).on(loadPostsFx.doneData, (_prevState, posts) => posts);
+const posts$ = createStore<unknown[]>([]).on(loadPostsFx.doneData, (_prevState, posts) => posts);
 
 posts$.watch((posts) => {
   console.log(`Posts amount: ${posts.length}`);
