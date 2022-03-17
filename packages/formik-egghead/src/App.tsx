4ef73378da032fa30c08d8b6212815964680d07a
Aleksei Arro <polsck@gmail.com>
2022-03-17 18:26:10 +0300

    feat: intergate with Formik

--- packages/formik-egghead/src/App.tsx
+++ packages/formik-egghead/src/App.tsx
@@ -1,36 +1,27 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
 
 type State = {
   items: string[];
-  value: string;
 };
 
 const INITIAL_STATE: State = {
   items: [],
-  value: '',
 };
 
 const FIELD_ID = 'value-field';
 
+const VALIDATION_SCHEMA = yup.object().shape({
+  value: yup.string().required('Cannot be empty'),
+});
+
 export function App() {
   const [state, setState] = React.useState(INITIAL_STATE);
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    setState((prevState) => ({
-      items: [...prevState.items, state.value],
-      value: '',
-    }));
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState((prevState) => ({ ...prevState, value: event.target.value }));
-  };
-
   return (
     <div>
-      <h2>Regular Maintenance</h2>
+      <div className="title">Regular Maintenance</div>
 
       {state.items.length > 0 ? (
         <ul>
@@ -42,12 +33,33 @@ export function App() {
         <div>No items</div>
       )}
 
-      <form onSubmit={handleSubmit}>
-        <label htmlFor={FIELD_ID}>New item</label>
-        <input id={FIELD_ID} type="text" name="item" value={state.value} onChange={handleChange} />
+      <Formik
+        initialValues={{ value: '' }}
+        validationSchema={VALIDATION_SCHEMA}
+        onSubmit={(values, actions) => {
+          setState((prevState) => ({
+            items: [...prevState.items, values.value],
+          }));
+          actions.resetForm();
+        }}
+      >
+        {({ errors }) => (
+          <Form className="form">
+            <label className="label" htmlFor={FIELD_ID}>
+              New item
+            </label>
 
-        <button type="submit">Add</button>
-      </form>
+            <div className="field-wrapper">
+              <Field id={FIELD_ID} type="text" name="value" />
+              <button className="button" type="submit" disabled={!!errors.value}>
+                Add
+              </button>
+            </div>
+
+            <ErrorMessage name="value" className="error" component="div" />
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
