b43274c94720394ddb3cfb9e6885dfaac90e3837
Aleksei Arro <polsck@gmail.com>
2022-03-23 19:53:36 +0300

    feat: add cases with ref and setState

--- packages/nir-kaufman_react-nyc-2018/public/app.js
+++ packages/nir-kaufman_react-nyc-2018/public/app.js
@@ -1,16 +1,35 @@
 'use strict';
 
-const Title = (props) => {
-  console.log('Title props', props);
-  return React.createElement('h1', null, props.name);
-};
-
 class App extends React.Component {
   state = { title: 'Hello world!' };
+  titleRef = React.createRef();
+
+  componentDidMount() {
+    console.log('this.titleRef', this.titleRef);
+  }
+
+  changeTitle() {
+    this.updater.enqueueSetState(this, { title: 'Hello world!' });
+    console.log('this.state', this.state);
+  }
 
   render() {
-    const el = React.createElement(Title, { title: this.state.title });
-    console.log('App el', el);
+    const el = React.createElement(
+      'h1',
+      null,
+      React.createElement('span', null, this.state.title),
+      React.createElement(
+        'button',
+        {
+          onClick: () => {
+            this.changeTitle();
+          },
+        },
+        'click',
+      ),
+    );
+
+    console.log('App element', el);
 
     return el;
   }
